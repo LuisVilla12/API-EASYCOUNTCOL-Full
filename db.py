3a1f457c1f0d292ed56a1f62d44cd1ba4687f397
LuisVilla12 <luisji2312ia@gmail.com>
2026-04-23 16:57:15 -0600

    udpate v1

--- db.py
+++ db.py
@@ -6,7 +6,7 @@ DB_CONFIG = {
     'user': os.getenv('DB_USER', 'root'),
     'password': os.getenv('DB_PASSWORD', 'lkqaz923'),
     'database': os.getenv('DB_NAME', 'easycountcol'),
-    'port': int(os.getenv('DB_PORT', '3306')),
+    'port': int(os.getenv('DB_PORT', '3307')),
     'charset': 'utf8mb4',
     'collation': 'utf8mb4_general_ci'
 }
